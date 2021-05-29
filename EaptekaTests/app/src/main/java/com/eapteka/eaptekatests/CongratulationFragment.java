149500ad968c8c8aea22dff56fb6b2487892ccae
Исмагилова Дарья <armi.din1902@gmail.com>
2021-05-29 14:25:08 +0300

    confetti

--- EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/CongratulationFragment.java
+++ EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/CongratulationFragment.java
@@ -1,12 +1,19 @@
 package com.eapteka.eaptekatests;
 
+import android.os.Build;
 import android.os.Bundle;
 
+import androidx.annotation.RequiresApi;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import nl.dionsegijn.konfetti.KonfettiView;
+import nl.dionsegijn.konfetti.emitters.StreamEmitter;
+import nl.dionsegijn.konfetti.models.Shape;
+import nl.dionsegijn.konfetti.models.Size;
 
 public class CongratulationFragment extends Fragment {
 
@@ -19,6 +26,7 @@ public class CongratulationFragment extends Fragment {
         super.onCreate(savedInstanceState);
     }
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -27,6 +35,19 @@ public class CongratulationFragment extends Fragment {
         view.setOnClickListener(v -> {
             getActivity().getSupportFragmentManager().popBackStack();
         });
+        final KonfettiView konfettiView = view.findViewById(R.id.view_confetti);
+        konfettiView.build()
+                .addColors(getActivity().getResources().getColor(R.color.teal_700),
+                        getActivity().getResources().getColor(R.color.teal_200),
+                        getActivity().getResources().getColor(R.color.purple_500))
+                .setDirection(0.0, 359.0)
+                .setSpeed(1f, 5f)
+                .setFadeOutEnabled(true)
+                .setTimeToLive(2000L)
+                .addShapes(Shape.Square.INSTANCE, Shape.Circle.INSTANCE)
+                .addSizes(new Size(12, 5f))
+                .setPosition(0, (float)konfettiView.getWidth(), 0f, 0f)
+                .streamFor(400, StreamEmitter.INDEFINITE);
         return view;
     }
 }
