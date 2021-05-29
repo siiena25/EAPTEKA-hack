a7500acb62b754511bbec56e59532866e54c92e6
Julia Kostyunina <54817988+siiena25@users.noreply.github.com>
2021-05-29 12:07:58 +0300

    test result 0.1v (#5)
    Co-authored-by: Исмагилова Дарья <armi.din1902@gmail.com>

--- EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/MainActivity.java
+++ EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/MainActivity.java
@@ -1,6 +1,7 @@
 package com.eapteka.eaptekatests;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -10,6 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /*getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new TestResultFragment(), "000")
+                .addToBackStack(null)
+                .commitAllowingStateLoss();*/
     }
 
 
