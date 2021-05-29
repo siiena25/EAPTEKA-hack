7864f7e346b094492185864c19c09258f88648ea
Исмагилова Дарья <Armi.din1902@gmail.com>
2021-05-29 16:05:34 +0300

    Test result (#9)
    * test result 0.1v
    
    * merge
    
    * confetti
    
    * finalTest to testResult
    
    * animation in TestResultFragment
    
    * add bundle with const args
    
    Co-authored-by: Julia Kostyunina <54817988+siiena25@users.noreply.github.com>

--- EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/MainActivity.java
+++ EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
 
@@ -45,6 +46,13 @@ public class MainActivity extends AppCompatActivity {
                 logger.log(pill.substance);
             }
         };
+
+        /*getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.container, new TestResultFragment(), "000")
+                .addToBackStack(null)
+                .commitAllowingStateLoss();*/
+
 
         pillViewModel.updatePill("Arbidol");
         pillViewModel.getPill().observe(this, obs);
