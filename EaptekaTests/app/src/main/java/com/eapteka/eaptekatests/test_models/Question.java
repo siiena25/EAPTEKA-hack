310fb4bb818c568a61bcd31d8577ae5810a3520a
IDolgopolov <dendolg1@mail.ru>
2021-05-30 12:02:42 +0300

    fix title

--- EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/test_models/Question.java
+++ EaptekaTests/app/src/main/java/com/eapteka/eaptekatests/test_models/Question.java
@@ -6,7 +6,7 @@ import java.util.ArrayList;
 
 public class Question {
 
-    @SerializedName("Questions")
+    @SerializedName("title")
     public String title; //"Срок годности"
 
     @SerializedName("description")
